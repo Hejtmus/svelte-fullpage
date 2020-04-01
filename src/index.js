df9a9e0c3c486f0ea67adc022ecc2b5033c61c23
Filip Holcik <filip.holcik.official@gmil.com>
2020-04-01 11:30:17 +0200

    Excluded dynamic version of component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
-//export { default as default } from './Component.svelte';
-export { default as FullpageDynamic } from './FullpageDynamic.svelte';
-export { default as FullpageSectionDynamic } from './FullpageSectionDynamic.svelte';
-export { default as FullpageStatic } from './FullpageStatic.svelte';
-export { default as FullpageSectionStatic } from './FullpageSectionStatic.svelte';
+export { default as Fullpage } from './FullpageStatic.svelte';
+export { default as FullpageSection } from './FullpageSectionStatic.svelte';
