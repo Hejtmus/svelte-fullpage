3ed7bef723696c829f0188b9c7b2429462450574
Filip Holcik <filip.holcik.official@gmil.com>
2020-04-02 23:59:08 +0200

    Migrated repo page from Sapper to Svelte

--- docs/svelte-fullpage/__sapper__/dev/client/client.18f6623b.js
+++
@@ -1,2 +0,0 @@
-import './client.204c40ab.js';
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY2xpZW50LjE4ZjY2MjNiLmpzIiwic291cmNlcyI6W10sInNvdXJjZXNDb250ZW50IjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiJ9
