74e11711923a6ad457767fe5821afe1267fd0551
Filip Holčík <filip.holcik.official@gmail.com>
2022-10-21 22:44:38 +0200

    Rebuilt demo page

--- docs/_app/immutable/chunks/1-86428e1d.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-295902ec.js";export{t as component};
--- docs/_app/immutable/chunks/2-40260e43.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-a214706c.js";export{t as component};
