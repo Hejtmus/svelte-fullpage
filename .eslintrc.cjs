2cf1753ad65bbc0b4fac09d94e38190a937174b5
Filip Holčík <filip.holcik.official@gmail.com>
2023-07-05 17:49:39 +0200

    chore: updated dependencies (svelte to 4)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,13 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'standard'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	extends: ['eslint:recommended', 'plugin:svelte/recommended', 'plugin:@typescript-eslint/recommended', 'standard'],
+	plugins: ['@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	overrides: [{
+		files: ['*.svelte'],
+		parser: 'svelte-eslint-parser'
+	}],
 	settings: {
 		'svelte3/typescript': () => require('typescript')
 	},
